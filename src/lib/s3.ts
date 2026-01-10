8cb6ab6fb808c537babe33b68a949cd7a28977f2
Sven Messmer <me@thesven.dev>
2026-01-10 10:42:31 +0100

    fix: s3 force path style

--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -1,12 +1,13 @@
 import { S3Client } from "@aws-sdk/client-s3";
 
 export const s3 = new S3Client({
-  region: process.env.S3_REGION || "auto",
-  endpoint: process.env.S3_ENDPOINT,
-  credentials: {
-    accessKeyId: process.env.S3_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY!,
-  },
+	region: process.env.S3_REGION || "auto",
+	endpoint: process.env.S3_ENDPOINT,
+	forcePathStyle: true,
+	credentials: {
+		accessKeyId: process.env.S3_ACCESS_KEY_ID!,
+		secretAccessKey: process.env.S3_SECRET_ACCESS_KEY!,
+	},
 });
 
 export const BUCKET_NAME = process.env.S3_BUCKET_NAME!;
